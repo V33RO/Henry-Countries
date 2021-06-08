7f86e4e6bdd0624caf4b69e75892470068d16a34
V33RO <cardozoveronicaro@gmail.com>
2021-06-08 20:37:10 -0300

    base andando

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -5,7 +5,7 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
     id:{
-      type:DataTypes.STRING,
+      type:DataTypes.UUID,
       primaryKey:true
     },
     name: {
